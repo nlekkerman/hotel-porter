3c39c5fb987760a75d1a63649aa5b73d4e53a8eb
nlekkerman <nlekkerman@gmail.com>
2025-05-13 11:49:53 +0100

    Fixed allowed host url.

--- porterproject/settings.py
+++ porterproject/settings.py
@@ -35,11 +35,16 @@ DATABASES = {
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,hotel-porter-d25ad83b12cf.herokuapp.com').split(',')
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'hotel-porter-d25ad83b12cf.herokuapp.com',
+]
+
 
 CSRF_TRUSTED_ORIGINS = [
     'https://*.gitpod.io',
-    'https://*.herokuapp.com',
+    'https://*hotel-porter-d25ad83b12cf.herokuapp.com',
     "http://localhost:5173",
     
     
