37c3d46363b8b0bddcc8b8756072b9608213b713
nlekkerman <nlekkerman@gmail.com>
2025-05-13 12:24:24 +0100

    Switch to PostgreSQL for Heroku

--- porterproject/settings.py
+++ porterproject/settings.py
@@ -86,10 +86,7 @@ WSGI_APPLICATION = 'porterproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
 
 
